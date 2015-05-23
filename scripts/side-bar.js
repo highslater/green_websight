e27203718d20d43b96f4f85a027ca0e036698cc0
highslater <highslater@hotmail.com>
2015-05-22 20:51:38 -0400

    Add about and contact

--- scripts/side-bar.js
+++ scripts/side-bar.js
@@ -1,7 +1,7 @@
 document.write("<div class='list'>");
     document.write("<ul>");
 	          document.write('<li><a href="http://codepen.io/highslater/">My CodePen</a></li>');
-              document.write('<li><a href="https://github.com/">GitHub</a></li>');
+              document.write('<li><a href="https://github.com/highslater">My GitHub</a></li>');
 	          document.write('<li><a href="http://www.codecademy.com/">CodeAcademy</a></li>');
 	          document.write('<li><a href="http://codepen.io/">CodePen</a></li>');
 	          document.write('<li> <a href="http://www.favicomatic.com/">FavIcomatic</a></li>'); 
