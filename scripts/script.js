e30b54bedb75553c7e84fe71bdb878cc085e74d2
highslater <highslater@hotmail.com>
2015-05-25 15:00:23 -0400

    Add about and contact

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     $('.hamburger').on('click', function(){
         $(this).toggleClass('transform');
         $('.list').toggle();
+        $('.listBack').toggle();
     });
     
     
