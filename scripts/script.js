aa1fac475611a5581b3467bfa2c3af001f13c2e6
highslater <highslater@hotmail.com>
2015-05-30 00:09:55 -0400

    Add about and contact

--- scripts/script.js
+++ scripts/script.js
@@ -3,9 +3,27 @@
 
 $(document).ready(function () {
     'use strict';
+    
     $('.hamburger').click(function () {
         $(this).toggleClass('transform');
-        $(".list, .listBack").slideToggle(1000);
+        $('.list, .listBack').slideToggle(1000);
     });
+    
+});
+
+$(document).ready(function () {
+    'use strict';
+    
+    
+    var $link = $('.listBack');
+    
+    $link.mouseenter(function () {
+        $('#testDiv').slideDown();
+    });
+    
+    $link.mouseleave(function () {
+        $('#testDiv').slideUp();
+    });
+    
     
 });
